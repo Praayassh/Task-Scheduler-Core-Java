b1eb198bf3c52cf9982310168fb20797443d67ed
Praayassh <prayashs102@gmail.com>
2024-05-20 20:22:34 +0545

    Initial commit for this project

--- src/np/com/prayashsapkota/taskmanager/base/NewTask.java
+++ src/np/com/prayashsapkota/taskmanager/base/NewTask.java
@@ -1,9 +1,9 @@
 package np.com.prayashsapkota.taskmanager.base;
 
 public class NewTask {
-    private String taskDescription;
-    private String dueDate;
-    private String priority;
+    private final String taskDescription;
+    private final String dueDate;
+    private final String priority;
 
     public NewTask(String taskDescription, String dueDate, String priority) {
         this.taskDescription = taskDescription;
@@ -21,18 +21,6 @@ public class NewTask {
 
     public String getPriority() {
         return priority;
-    }
-
-    public void setTaskDescription(String taskDescription) {
-        this.taskDescription = taskDescription;
-    }
-
-    public void setDueDate(String dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
     }
 
     public String toString() {
